7bf65c8a00b9425aea18d695a2b937b5ec0eac8c
sknutter <stephen.knutter@gmail.com>
2014-12-18 13:55:12 -0700

    New JS File

--- js/beer.js
+++ js/beer.js
@@ -1,4 +1,4 @@
-
+var storedMarker = Array();
 function createXmlRequest(){
 	var xmlHttp;
 	try{
@@ -23,6 +23,84 @@ function createXmlRequest(){
 	}
 }
 
+var xmlHttp = createXmlRequest();
+
+function displaybrews(){
+	if(xmlHttp.readyState == 4){
+		if(xmlHttp.status == 200 || xmlHttp.status == 304){
+			try{
+				var root = xmlHttp.responseXML.documentElement;
+				var brews = root.getElementsByTagName("brews");
+				var mapOptions ={
+					zoom: 12,
+					zoomControlOptions: {
+						position: google.maps.ControlPosition.LEFT_BOTTOM,
+					},
+					streetViewControlOptions: {
+						position: google.maps.ControlPosition.LEFT_BOTTOM,
+					},
+					center: new google.maps.LatLng(39.737567, -104.984718),
+					scrollwheel: false,
+					navigationControl: false,
+					mapTypeControl: false,
+					panControl: false,
+			};
+	
+			map = new google.maps.Map(document.getElementById('gmap'), mapOptions);
+			/*map.setZoom(12);*/
+			
+			var infoBubble = new InfoBubble({
+						map: map,
+						backgroundColor: '#ffffff',
+						borderRadius: 4,
+						maxWidth: 260,
+						minWidth: 260,
+						maxHeight: 45,
+						minHeight: 45,
+						arrowSize: 10,
+						borderWidth: 2,
+						borderColor: '#e95404',
+						disableAutoPan: false,
+						arrowPosition: 50,
+						backgroundClassName: 'phoney',
+						arrowStyle: 1
+				});
+				
+				/*var infoWindow = new google.maps.InfoWindow;*/
+				
+				for(var i=0; i < brews.length; i++){
+					var brew = brews[i].getAttribute("brew");
+					var address = brews[i].getAttribute("address");
+					var id = brews[i].getAttribute("id");
+					var point = new google.maps.LatLng(
+						parseFloat(brews[i].getAttribute("lat")),
+						parseFloat(brews[i].getAttribute("lng"))
+					);
+					var html = "<p class='jsBrew'><a href='#' id="+id+">"+brew+"</a></p>";
+					html += "<p class='jsAddress'>"+address+"</p>";
+					
+					var marker = new google.maps.Marker({
+						map: map,
+						position: point,
+						icon: './images/beer-mug.png'
+					});
+					
+					/*brewList.push(brew);*/
+					bindMarker(marker, map, infoBubble, html);
+					appendListing(html);
+					storedMarker[id] = marker;
+					if(i == brews.length-1){
+						$('body').click();
+					}
+		      }
+			  
+			}catch(e){
+			  alert("Error recieving server response");
+		  }
+		}
+	 }
+   }
+
 function init(){
 	$('#listing').empty();
 	/*RESIZE MAP*/
@@ -42,100 +120,26 @@ function init(){
 	
 	
 	/*var infoWindow = new google.maps.InfoWindow;*/
-	var xmlHttp = createXmlRequest();
 	if(xmlHttp){
 		xmlHttp.open("GET", "beer_db.php", true);
 		xmlHttp.onreadystatechange = displaybrews;
 		xmlHttp.send(null);
 	}
 	
-	function displaybrews(){
-	if(xmlHttp.readyState == 4){
-		if(xmlHttp.status == 200){
-			try{
-				var root = xmlHttp.responseXML.documentElement;
-				var brews = root.getElementsByTagName("brews");
-				
-				var mapOptions ={
-					zoom: 8,
-					zoomControlOptions: {
-						position: google.maps.ControlPosition.LEFT_BOTTOM
-					},
-					streetViewControlOptions: {
-						position: google.maps.ControlPosition.LEFT_BOTTOM
-					},
-					center: new google.maps.LatLng(39.737567, -104.984718),
-					scrollwheel: false,
-					navigationControl: false,
-					mapTypeControl: false,
-					panControl: false,
-			};
 	
-			map = new google.maps.Map(document.getElementById('gmap'), mapOptions);
-			map.setZoom(12);
-			storedMarker = Array();
-			var infoBubble = new InfoBubble({
-						map: map,
-						backgroundColor: 'rgba(255, 204, 15, .9)',
-						borderRadius: 4,
-						maxWidth: 260,
-						minWidth: 260,
-						maxHeight: 45,
-						minHeight: 45,
-						arrowSize: 10,
-						borderWidth: 2,
-						borderColor: '#e95404',
-						disableAutoPan: false,
-						arrowPosition: 50,
-						backgroundClassName: 'phoney',
-						arrowStyle: 1
-				});
-				
-				for(var i=0; i < brews.length; i++){
-					var brew = brews[i].getAttribute("brew");
-					var address = brews[i].getAttribute("address");
-					var id = brews[i].getAttribute("id");
-					var point = new google.maps.LatLng(
-						parseFloat(brews[i].getAttribute("lat")),
-						parseFloat(brews[i].getAttribute("lng"))
-					);
-					var html = "<p class='jsBrew'><a href='#' id="+id+">"+brew+"</a></p>";
-					html += "<p class='jsAddress'>"+address+"</p>";
-					
-					var marker = new google.maps.Marker({
-						map: map,
-						position: point,
-						icon: './images/beer-mug.png'
-					});
-					
-					bindMarker(marker, map, html);
-					appendListing(html);
-					storedMarker[id] = marker;
-					if(i === brews.length-1){
-						$('body').click();
-					}
-				}
-				
-				function bindMarker(marker, map, html){
-				google.maps.event.addListener(marker,'click',function() {
-				infoBubble.close();
-				infoBubble.setContent(html);
-				infoBubble.open(map, marker);
-				});
-		      }
-			}catch(e){
-			  alert("Error recieving server response");
-		  }
-		}
-	 }
-   }
-   
-
    var docWidth = document.documentElement.clientWidth;
    if(docWidth <= 800){
 		$("#listing").animate({right:-300});
    }
 
+}
+
+function bindMarker(marker, map, infoBubble, html){
+		google.maps.event.addListener(marker, 'click', function() {
+			infoBubble.close()
+			infoBubble.setContent(html)
+			infoBubble.open(map, marker)
+		});
 }
 
 function appendListing(html){
@@ -149,5 +153,6 @@ function appendListing(html){
 		});
 	}
 }
+
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', init);
